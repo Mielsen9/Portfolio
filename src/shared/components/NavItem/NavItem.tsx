76c00562889c7238823aa62068e8c4553795dead
Petro <worldinthegame@gmail.com>
2025-04-01 14:02:53 +0300

    create NawItems

---
+++ src/shared/components/NavItem/NavItem.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as s from "./NavItem.module.scss"
+// Type
+type PropsType = {
+
+};
+// NavItem
+const NavItem: React.FC<PropsType> = React.memo((p) => {
+
+	// Return
+	return (
+		<div>
+
+		</div>
+	)
+});
+
+export default NavItem

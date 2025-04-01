99a26626a4fd2c7895351efa6b101d49e3a1c309
Petro <worldinthegame@gmail.com>
2025-04-01 20:03:13 +0300

    create intro components

---
+++ src/widgets/Intro/Intro.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as s from "./Intro.module.scss"
+// Type
+type PropsType = {
+
+};
+// Intro
+const Intro: React.FC<PropsType> = React.memo((p) => {
+
+	// Return
+	return (
+		<div className={s.conteiner}>
+
+		</div>
+	)
+});
+
+export default Intro

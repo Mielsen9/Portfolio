cc4d4679fe658dd8320f166dd3ab158d358f07d6
Petro <worldinthegame@gmail.com>
2025-04-01 20:03:40 +0300

    add intro components to App

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import * as s from "./App.module.scss";
 import Header from "@/widgets/Header/Header";
+import Intro from "@/widgets/Intro/Intro";
 
 // App
 const App:React.FC = () => {
     return (
-        <div className={s.wrapper}>
+        <div>
             <Header/>
+            <Intro/>
         </div>
     );
 };

7cab3a171f32b14e78cad9bb4b5dd3f6efe4efac
Petro <worldinthegame@gmail.com>
2025-04-26 15:48:07 +0300

    add page Main component

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import * as s from "./Main.module.scss"
+type PropsType = {
+
+};
+// Main
+const Main: React.FC<PropsType> = React.memo((p) => {
+
+	// Return
+	return (
+		<div>
+
+		</div>
+	)
+});
+
+export default Main

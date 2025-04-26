d7e08054b662708f8e32dc3d034beca8820c3d3a
Petro <worldinthegame@gmail.com>
2025-04-26 15:31:47 +0300

    add widget Skills component

---
+++ src/widgets/Skills/Skills.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import * as s from "./Skills.module.scss"
+// Type
+type PropsType = {
+
+};
+// Skills
+const Skills: React.FC<PropsType> = React.memo((p) => {
+
+	// Return
+	return (
+		<div>
+
+		</div>
+	)
+});
+
+export default Skills

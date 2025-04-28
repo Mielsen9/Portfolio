e54a26cd456be1f2efc77a8df1d60f93969822ef
Petro <worldinthegame@gmail.com>
2025-04-28 14:18:49 +0300

    add ContactMe component

---
+++ src/widgets/ContactMe/ContactMe.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as s from "./ContactMe.module.scss"
+
+// ContactMe
+const ContactMe: React.FC = (p) => {
+
+	// Return
+	return (
+		<div>
+
+		</div>
+	)
+};
+
+export default ContactMe

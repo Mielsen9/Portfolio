364fae075d15581ec479cb86d8fdf796271c4d1e
Petro <worldinthegame@gmail.com>
2025-04-29 19:51:34 +0300

    add footer components

---
+++ src/widgets/Footer/Footer.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import * as s from "./Footer.module.scss"
+import Logo from "@/shared/ui/Logo/Logo";
+// Type
+type PropsType = {
+
+};
+// Footer
+const Footer: React.FC<PropsType> = React.memo((p) => {
+
+	// Return
+	return (
+		<section className={s.footer}>
+			<div className={s.wrapper}>
+				<div className={s.container}>
+					<div className={s.logo}>
+						<Logo/>
+					</div>
+					<p>© 2035 by Petro Zvir. </p>
+				</div>
+			</div>
+		</section>
+	)
+});
+
+export default Footer
